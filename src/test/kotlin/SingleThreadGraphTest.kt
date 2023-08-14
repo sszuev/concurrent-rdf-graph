dcbc4d2aec23789b6b90c0a9cbc0286d6036c6ea
sszuev <sss.zuev@gmail.com>
2023-08-14 14:54:25 +0300

    rdf-graphs: add test on `ConcurrentModificationException`

--- src/test/kotlin/SingleThreadGraphTest.kt
+++ src/test/kotlin/SingleThreadGraphTest.kt
@@ -2,6 +2,10 @@ package com.github.sszuev.graphs
 
 import com.github.sszuev.graphs.scenarious.ReadOperationsTestData
 import com.github.sszuev.graphs.scenarious.testModifyAndRead
+import org.apache.jena.graph.Node
+import org.apache.jena.graph.NodeFactory
+import org.apache.jena.graph.Triple
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 
@@ -34,4 +38,24 @@ internal class SingleThreadGraphTest {
             numTriplesToDelete = 21,
         )
     }
+
+    @ParameterizedTest
+    @EnumSource(TestGraphFactory::class)
+    fun `test ConcurrentModificationException is not thrown for concurrent graphs (acceptance test)`(factory: TestGraphFactory) {
+        val graph = factory.load("/pizza.ttl")
+        val subject = NodeFactory.createURI("http://www.co-ode.org/ontologies/pizza/pizza.owl#hasTopping")
+        val it = graph.find(Triple.create(subject, Node.ANY, Node.ANY))
+        it.next()
+        it.next()
+        graph.add(Triple.create(NodeFactory.createURI("42"), Node.ANY, Node.ANY))
+        it.next()
+        it.next()
+        graph.add(Triple.create(subject, Node.ANY, Node.ANY))
+        if (factory == TestGraphFactory.MEM_GRAPH) {
+            Assertions.assertThrows(ConcurrentModificationException::class.java) { it.next() }
+        } else {
+            it.next()
+        }
+    }
+
 }
